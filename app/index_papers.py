89047b2416ec2217ed595706b67e4ea91bcbcd60
Ioannis Antoniadis <johneegr@gmail.com>
2020-04-04 22:22:33 +0300

    Enhancement in index_papers.py script #1

--- app/index_papers.py
+++ app/index_papers.py
@@ -1,47 +1,157 @@
 """
-  Index json papers into Elasticsearch
-  - The original json files need to have beeen preprocessed 
-    and placed in output directory
-  - Elasticsearch server must be up 'n' running
+  Elasticsearch client for:
+    - index creation with custom mapping
+    - indexing of papers
+  Prerequisites:
+    - elasticsearch server must be up 'n' running
+    - no index with the same name must exist
+    - paper json documents must be provided in 'data/processed' directory
 """
-from elasticsearch_dsl import Index, connections, analyzer, tokenizer, Mapping
+from elasticsearch_dsl import connections, analyzer, tokenizer, Index, Mapping, Nested, Text, Object, Long, Field, Keyword
 import json
 import os
+
+# Define index name
+indexName = 'papers'
 
 # Default connection and Elasticsearch client
 connections.create_connection()
 es = connections.get_connection()
 
-# Analyzer
-my_analyzer = analyzer('my_analyzer',
-                       tokenizer=tokenizer(
-                           'trigram', 'nGram', min_gram=3, max_gram=3),
-                       filter=['lowercase']
-                       )
+# Check if index already exists
+i = Index(indexName)
+index_exists = i.exists()
 
-m = Mapping.from_es('first_draft', using='default')
+if not index_exists:
+    # Define analyzer
+    my_analyzer = analyzer('my_analyzer', tokenizer=tokenizer(
+        'trigram', 'nGram', min_gram=3, max_gram=3), filter=['lowercase'])
 
-# Index
-i = Index('new_draft')
-i.analyzer(my_analyzer)
-i.mapping(m)
-i.create()
+    # Define mapping
+    m = Mapping()
 
-# Indexing
-outputDir = 'E:/COVID-19_test/'
+    # abstract
+    m.field('abstract', Field(
+        properties={
+            'section': Text(fields={'keyword': Keyword(ignore_above=256)}),
+            'text': Text(fields={'keyword': Keyword(ignore_above=256)})
+        })
+    )
+    # back_matter
+    m.field('back_matter', Field(
+        properties={
+            'section': Text(fields={'keyword': Keyword(ignore_above=256)}),
+            'text': Text(fields={'keyword': Keyword(ignore_above=256)})
+        })
+    )
+    # bib_entries
+    m.field('bib_entries', Field(
+        properties={
+            'authors': Field(
+                properties={
+                    'first': Text(fields={'keyword': Keyword(ignore_above=256)}),
+                    'last': Text(fields={'keyword': Keyword(ignore_above=256)}),
+                    'middle': Text(fields={'keyword': Keyword(ignore_above=256)}),
+                    'suffix': Text(fields={'keyword': Keyword(ignore_above=256)})
+                }
+            ),
+            'id': Text(fields={'keyword': Keyword(ignore_above=256)}),
+            'issn': Text(fields={'keyword': Keyword(ignore_above=256)}),
+            'other_ids': Object(),
+            'pages': Text(fields={'keyword': Keyword(ignore_above=256)}),
+            'ref_id': Text(fields={'keyword': Keyword(ignore_above=256)}),
+            'title': Text(fields={'keyword': Keyword(ignore_above=256)}),
+            'venue': Text(fields={'keyword': Keyword(ignore_above=256)}),
+            'volume': Text(fields={'keyword': Keyword(ignore_above=256)}),
+            'year': Long()
+        })
+    )
+    # body_text
+    m.field('body_text', Nested(
+        include_in_parent=True,
+        properties={
+            'cite_spans': Field(
+                properties={
+                    'end': Long(),
+                    'ref_id': Text(fields={'keyword': Keyword(ignore_above=256)}),
+                    'start': Long(),
+                    'text': Text(fields={'keyword': Keyword(ignore_above=256)})
+                }
+            ),
+            'ref_spans': Field(
+                properties={
+                    'end': Long(),
+                    'ref_id': Text(fields={'keyword': Keyword(ignore_above=256)}),
+                    'start': Long(),
+                    'text': Text(fields={'keyword': Keyword(ignore_above=256)})
+                }
+            ),
+            'section': Text(fields={'keyword': Keyword(ignore_above=256)}),
+            'text': Text(fields={'keyword': Keyword(ignore_above=256)})
+        }
+    ))
+    # metadata
+    m.field('metadata', Field(
+        properties={
+            'authors': Field(
+                properties={
+                    'affiliation': Field(
+                        properties={
+                            'institution': Text(fields={'keyword': Keyword(ignore_above=256)}),
+                            'laboratory': Text(fields={'keyword': Keyword(ignore_above=256)}),
+                            'location': Field(
+                                properties={
+                                    'country': Text(fields={'keyword': Keyword(ignore_above=256)}),
+                                    'settlement': Text(fields={'keyword': Keyword(ignore_above=256)})
+                                }
+                            )
+                        }
+                    ),
+                    'email': Text(fields={'keyword': Keyword(ignore_above=256)}),
+                    'first': Text(fields={'keyword': Keyword(ignore_above=256)}),
+                    'last': Text(fields={'keyword': Keyword(ignore_above=256)}),
+                    'suffix': Text(fields={'keyword': Keyword(ignore_above=256)})
+                }
+            ),
+            'title': Text(fields={'keyword': Keyword(ignore_above=256)})
+        })
+    )
+    # paper_id
+    m.field('paper_id', 'text', fields={'keyword': Keyword(ignore_above=256)})
+    # ref_entries
+    m.field('ref_entries', Field(
+        properties={
+            'id': Text(fields={'keyword': Keyword(ignore_above=256)}),
+            'text': Text(fields={'keyword': Keyword(ignore_above=256)}),
+            'type': Text(fields={'keyword': Keyword(ignore_above=256)})
+        })
+    )
+
+    # Create index
+    i.analyzer(my_analyzer)
+    i.mapping(m)
+    i.create()
+    print('Created index', indexName)
+else:
+    print('Index', indexName, 'already exists, skipping creation.')
+
+# Index documents
+inputDir = 'data/processed'
 count = 0
-for filename in os.listdir(outputDir):
+for filename in os.listdir(inputDir):
     if filename.endswith(".json"):
         # increment counter
         count += 1
         # read file
-        with open(os.path.join(outputDir, filename), 'r') as myfile:
+        with open(os.path.join(inputDir, filename), 'r') as myfile:
             data = myfile.read()
             # load into json object
             doc = json.loads(data)
             # index to Elasticsearch
-            es.index(index="new_draft", id=doc["paper_id"], body=doc)
+            es.index(index=indexName, id=doc["paper_id"], body=doc)
             print('Indexing document id:', count)
             myfile.close()
     else:
         continue
+
+print('Indexed', count, 'papers.')
